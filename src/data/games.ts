5da12b04f5e2faf0b7bb766eeee6f526016b412a
Rait Nigol <thetrollfacecompany@gmail.com>
2023-10-10 01:35:48 +0300

    add images for games

--- src/data/games.ts
+++ src/data/games.ts
@@ -10,25 +10,25 @@ export const games: Games[] = [
 		name: "Resident Evil 2",
 		description: "",
 		image: "/images/games/residentevil2.jpg",
-		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+		url: "https://store.steampowered.com/app/883710/Resident_Evil_2/"
 	},
 	{
 		name: "Resident Evil 3",
 		description: "",
 		image: "/images/games/residentevil3.jpg",
-		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+		url: "https://store.steampowered.com/app/952060/Resident_Evil_3/"
 	},
 	{
 		name: "Resident Evil 4 (2005)",
 		description: "",
 		image: "/images/games/residentevil4.jpg",
-		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+		url: "https://store.steampowered.com/app/254700/Resident_Evil_4_2005/"
 	},
 	{
 		'name': 'Dead Island', 
 		'description': '', 
 		'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/dead_island'
-	},
+    },
 	{'name': 'Dead Island Riptide', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/dead_island_riptide'},
 	{'name': 'DOOM', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/doom'},
 	{'name': 'DOOM Eternal', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/doom_eternal'},
@@ -42,25 +42,55 @@ export const games: Games[] = [
 	{'name': 'Resident Evil 5', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/resident_evil_5'},
 	{'name': 'Resident Evil 6', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/resident_evil_6'},
 	{'name': 'Stardew Valley', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/stardew_valley'},
-	{'name': 'Bleed 2', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/bleed_2'},
+	{
+	    'name': 'Bleed 2',
+	    'description': 'Bleed 2 is a relentless arcade action game featuring air-dodging, bullet-reflecting, and tons of bosses.',
+	    'image': '/images/games/bleed2.jpg',
+	    'url': 'https://store.steampowered.com/app/396350/Bleed_2/'
+    },
 	{'name': 'Bright Memory', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/bright_memory'},
 	{'name': 'Call of Juarez Gunslinger', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/call_of_juarez_gunslinger'},
 	{'name': 'Clustertruck', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/clustertruck'},
-	{'name': 'Cyber Hook', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/cyber_hook'},
+	{
+	    'name': 'Cyber Hook',
+	    'description': 'Cyber Hook is a fast-paced, 3D platformer that has you using parkour skills and a grappling hook to scale and ride walls, while fighting enemies and blasting blocks with a laser gun.',
+	    'image': '/images/games/cyberhook.jpg',
+	    'url': 'https://store.steampowered.com/app/1130410/Cyber_Hook/'
+    },
 	{'name': 'Cyberpunk 2077', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/cyberpunk_2077'},
 	{'name': 'DIRT Showdown', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/dirt_showdown'},
 	{'name': 'DOOM (1993)', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/doom_(1993)'},
-	{'name': "God's Trigger", 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': "https://placeholder.url/god's_trigger"},
+	{
+	    'name': "God's Trigger",
+	    'description': '',
+	    'image': '/images/games/godstrigger.jpg',
+	    'url': "https://store.steampowered.com/app/488730/Gods_Trigger/"
+	},
 	{'name': 'F.E.A.R. 2: Project Origin', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/f.e.a.r._2:_project_origin'},
 	{'name': "Hellblade: Senua's Sacrifice", 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': "https://placeholder.url/hellblade:_senua's_sacrifice"},
 	{'name': 'HITMAN', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/hitman'},
-	{'name': 'Hitman: Blood Money', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/hitman:_blood_money'},
-	{'name': 'Hitman: Absolution', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/hitman:_absolution'},
+	{
+	    'name': 'Hitman: Blood Money',
+	    'description': '',
+	    'image': '/images/games/hitmanbloodmoney.jpg',
+	    'url': 'https://store.steampowered.com/app/6860/Hitman_Blood_Money/'
+	},
+	{
+	    'name': 'Hitman: Absolution',
+	    'description': '',
+	    'image': '/images/games/hitmanabsolution.jpg',
+	    'url': 'https://store.steampowered.com/app/203140/Hitman_Absolution/'
+	},
 	{'name': 'In Fear I Trust', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/in_fear_i_trust'},
 	{'name': 'Just Cause 3', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/just_cause_3'},
 	{'name': 'Just Cause 4', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/just_cause_4'},
 	{'name': 'Late Shift', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/late_shift'},
-	{'name': 'Layers of Fear (2016)', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/layers_of_fear_(2016)'},
+	{
+	    'name': 'Layers of Fear (2016)',
+	    'description': 'Layers of Fear is a first-person psychedelic horror game with a heavy focus on story and exploration.',
+	    'image': '/images/games/layersoffear.png',
+	    'url': 'https://store.steampowered.com/app/391720/Layers_of_Fear_2016/'
+	},
 	{'name': 'Life is Strange', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/life_is_strange'},
 	{'name': 'Lovely Planet', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/lovely_planet'},
 	{'name': 'Mafia II (Classic)', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/mafia_ii_(classic)'},
@@ -69,24 +99,64 @@ export const games: Games[] = [
 	{'name': 'Portal', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/portal'},
 	{'name': 'POSTAL 2', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/postal_2'},
 	{'name': 'Ratchet & Clank: Rift Apart', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank:_rift_apart'},
-	{'name': 'Rise of the Tomb Raider', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/rise_of_the_tomb_raider'},
+	{
+	    'name': 'Rise of the Tomb Raider',
+	    'description': '',
+	    'image': '/images/games/riseofthetombraider.jpg',
+	    'url': 'https://store.steampowered.com/app/391220/Rise_of_the_Tomb_Raider/'
+	},
 	{'name': 'Saints Row: The Third', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/saints_row:_the_third'},
 	{'name': 'Saints Row IV', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/saints_row_iv'},
 	{'name': 'SEUM: Speedrunners from Hell', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/seum:_speedrunners_from_hell'},
-	{'name': 'Shadow of the Tomb Raider', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/shadow_of_the_tomb_raider'},
+	{
+	    'name': 'Shadow of the Tomb Raider',
+	    'description': '',
+	    'image': '/images/games/placeholder.jpeg',
+	    'url': 'https://placeholder.url/shadow_of_the_tomb_raider'
+	},
 	{'name': 'Sleeping Dogs', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/sleeping_dogs'},
 	{'name': 'Spore', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/spore'},
 	{'name': 'A Story About My Uncle', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/a_story_about_my_uncle'},
-	{'name': 'SUPERHOT', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/superhot'},
-	{'name': 'SUPERHOT: MIND CONTROL DELETE', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/superhot:_mind_control_delete'},
+	{
+	    'name': 'SUPERHOT',
+	    'description': 'SUPERHOT is the smash-hit FPS where time moves only when you move.',
+	    'image': '/images/games/superhot.jpg',
+	    'url': 'https://store.steampowered.com/app/322500/SUPERHOT/'
+	},
+	{
+	    'name': 'SUPERHOT: MIND CONTROL DELETE',
+	    'description': '',
+	    'image': '/images/games/superhotmindcontroldelete.jpg',
+	    'url': 'https://store.steampowered.com/app/690040/SUPERHOT_MIND_CONTROL_DELETE/'
+	},
 	{'name': 'Superliminal', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/superliminal'},
-	{'name': 'Tomb Raider', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/tomb_raider'},
+	{
+	    'name': 'Tomb Raider',
+	    'description': '',
+	    'image': '/images/games/tombraider.jpg',
+	    'url': 'https://store.steampowered.com/app/203160/Tomb_Raider/'
+	},
 	{'name': 'Grand Theft Auto V', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/grand_theft_auto_v'},
 	{'name': 'Ratchet & Clank (2002)', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank_(2002)'},
 	{'name': 'Ratchet & Clank: Going Commando', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank:_going_commando'},
-	{'name': 'Ratchet & Clank: Up Your Arsenal', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank:_up_your_arsenal'},
+	{
+	    'name': 'Ratchet & Clank: Up Your Arsenal',
+	    'description': '',
+	    'image': '/images/games/ratchetandclankupyourarsenal.jpg',
+	    'url': 'https://www.ign.com/games/ratchet-and-clank-up-your-arsenal'
+	},
 	{'name': 'Ratchet & Clank: Deadlocked', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank:_deadlocked'},
-	{'name': 'Ratchet & Clank: Size Matters', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank:_size_matters'},
-	{'name': 'Ratchet & Clank: Tools of Destruction', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank:_tools_of_destruction'},
-	{'name': 'Ratchet & Clank: A Crack In Time', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank:_a_crack_in_time'}
+	{
+	    'name': 'Ratchet & Clank: Size Matters',
+	    'description': '',
+	    'image': '/images/games/ratchetandclanksizematters.jpg',
+	    'url': 'https://www.ign.com/games/ratchet-clank-size-matters'
+	},
+	{'name': 'Ratchet & Clank Future: Tools of Destruction', 'description': '', 'image': '/images/games/placeholder.jpeg', 'url': 'https://placeholder.url/ratchet_&_clank:_tools_of_destruction'},
+	{
+	    'name': 'Ratchet & Clank Future: A Crack In Time',
+	    'description': '',
+	    'image': '/images/games/ratchetandclankacrackintime.jpg',
+	    'url': 'https://www.ign.com/games/ratchet-clank-future-a-crack-in-time'
+	}
 ];
