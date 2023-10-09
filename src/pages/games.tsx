8eca72422e2b7c6fd6c4afbfabb8c88c3731432f
Nigol <thetrollfacecompany@gmail.com>
2023-10-10 01:02:34 +0300

    Update games.tsx

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,46 +1,63 @@
 import { SparklesIcon } from "@heroicons/react/solid";
 import Image from "next/future/image";
+import { useEffect, useState } from "react";
 
 import GenericMeta from "../components/GenericMeta";
 import { games } from "../data/games";
 
+// Function to shuffle an array in place
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 export default function Games() {
-	return (
-		<>
-			<GenericMeta
-				title="Games ✨"
-				description="PC and console games I have completed"
-			/>
+  const [shuffledGames, setShuffledGames] = useState([]);
 
-			<h1 className="heading mb-2">
-				PC & Console games{" "}
-				<SparklesIcon className="ml-4 h-12 w-12 text-yellow-300" />
-			</h1>
+  useEffect(() => {
+    // Shuffle the games array and store it in the state
+    setShuffledGames(shuffleArray([...games]));
+  }, []);
 
-			<p className="text-lg mb-4">PC and console games I have completed.</p>
+  return (
+    <>
+      <GenericMeta
+        title="Games ✨"
+        description="PC and console games I have completed"
+      />
 
-			{games.map(({ name, description, image, url }, i) => (
-				<a
-					key={name}
-					href={url}
-					target="_blank"
-					rel="noreferrer noopener"
-					className="group relative flex items-center justify-items-start h-40 px-4 sm:px-8 mb-4 overflow-hidden bg-slate-900 rounded-lg before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-0 before:transition before:duration-300 hover:before:opacity-50"
-				>
-					<Image
-						src={image}
-						alt={name}
-						width={1200}
-						height={240}
-						className="absolute top-0 left-0 w-full h-full object-cover rounded-lg transition duration-300 group-hover:scale-[1.02]"
-						priority={i < 3}
-					/>
-					<div className="z-20 w-full transition duration-300 scale-95 opacity-0 group-hover:scale-100 group-hover:opacity-100">
-						<h2 className="font-bold text-3xl">{name}</h2>
-						<p className="text-lg">{description}</p>
-					</div>
-				</a>
-			))}
-		</>
-	);
+      <h1 className="heading mb-2">
+        PC & Console games{" "}
+        <SparklesIcon className="ml-4 h-12 w-12 text-yellow-300" />
+      </h1>
+
+      <p className="text-lg mb-4">PC and console games I have completed.</p>
+
+      {shuffledGames.map(({ name, description, image, url }, i) => (
+        <a
+          key={name}
+          href={url}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="group relative flex items-center justify-items-start h-40 px-4 sm:px-8 mb-4 overflow-hidden bg-slate-900 rounded-lg before:absolute before:inset-0 before:z-10 before:bg-black before:opacity-0 before:transition before:duration-300 hover:before:opacity-50"
+        >
+          <Image
+            src={image}
+            alt={name}
+            width={1200}
+            height={240}
+            className="absolute top-0 left-0 w-full h-full object-cover rounded-lg transition duration-300 group-hover:scale-[1.02]"
+            priority={i < 3}
+          />
+          <div className="z-20 w-full transition duration-300 scale-95 opacity-0 group-hover:scale-100 group-hover:opacity-100">
+            <h2 className="font-bold text-3xl">{name}</h2>
+            <p className="text-lg">{description}</p>
+          </div>
+        </a>
+      ))}
+    </>
+  );
 }
