e4a6d98d7726038142806f1fe3ca0895f09e6de6
Nigol <thetrollfacecompany@gmail.com>
2023-10-09 04:08:51 +0300

    Update tv_shows.tsx

--- src/pages/tv_shows.tsx
+++ src/pages/tv_shows.tsx
@@ -13,7 +13,7 @@ export default function TvShows() {
 			/>
 
 			<h1 className="heading mb-2">
-				Anime{" "}
+				TvShow{" "}
 				<SparklesIcon className="ml-4 h-12 w-12 text-yellow-300" />
 			</h1>
 
