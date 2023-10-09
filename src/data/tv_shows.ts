b73b675d89278ada061efc1cf3edf32dc3ce7328
Nigol <thetrollfacecompany@gmail.com>
2023-10-09 04:12:26 +0300

    Update tv_shows.ts

--- src/data/tv_shows.ts
+++ src/data/tv_shows.ts
@@ -7,9 +7,21 @@ export interface TvShows {
 
 export const tv_shows: TvShows[] = [
 	{
-		name: "Rait",
+		name: "Breaking Bad",
 		description: "",
-		image: "/images/anime/ai.jpg",
+		image: "/images/tv_shows/breakingbad.png",
+		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+	},
+	{
+		name: "Narcos",
+		description: "",
+		image: "/images/tv_shows/narcos.png",
+		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+	},
+	{
+		name: "Narcos: New Mexico",
+		description: "",
+		image: "/images/tv_shows/narcosnewmexico.png",
 		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
 	}
 ];
