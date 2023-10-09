50ec7e7d69829d44ab4cc5e0eafe740256dbb3bc
Nigol <thetrollfacecompany@gmail.com>
2023-10-09 04:22:18 +0300

    Create games.ts

---
+++ src/data/games.ts
@@ -0,0 +1,27 @@
+export interface Games {
+	name: string;
+	description: string;
+	image: string;
+	url: string;
+}
+
+export const games: Games[] = [
+	{
+		name: "Resident Evil 2",
+		description: "",
+		image: "/images/games/residentevil2.jpg",
+		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+	},
+	{
+		name: "Resident Evil 3",
+		description: "",
+		image: "/images/games/residentevil3.jpg",
+		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+	},
+	{
+		name: "Resident Evil 4 (2005)",
+		description: "",
+		image: "/images/games/residentevil4.jpg",
+		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+	}
+];
