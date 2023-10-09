2ecc6064700287b77a90fedffdb24c9cc0dfe53b
Nigol <thetrollfacecompany@gmail.com>
2023-10-09 04:00:02 +0300

    Create tv_shows.ts

---
+++ src/data/tv_shows.ts
@@ -0,0 +1,15 @@
+export interface TvShows {
+	name: string;
+	description: string;
+	image: string;
+	url: string;
+}
+
+export const tv_shows: TvShows[] = [
+	{
+		name: "Rait",
+		description: "",
+		image: "/images/anime/ai.jpg",
+		url: "https://anilist.co/anime/150672/Oshi-no-Ko/"
+	}
+];
