3728aa854dde07e34fb02d1d73e399bb54c2febd
Nigol <thetrollfacecompany@gmail.com>
2024-05-18 02:52:09 +0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,13 @@
 import Navbar from "./Navbar";
 import Transition from "./Transition";
 import { Analytics } from "@vercel/analytics/react";
-import { useRouter } from 'next/router';
-import BeholdWidget from "../components/BeholdWidget";
 
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
-  const router = useRouter();
-
   return (
     <div className="md:container md:px-0 mx-auto my-24 px-8 text-white text-xl">
       <Navbar />
       <main>
         <Transition>{children}</Transition>
-        {router.pathname === '/' && <BeholdWidget />}
       </main>
       <Analytics />
     </div>
