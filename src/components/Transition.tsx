053e3e08777d126f73b5676656329288fe72e35e
Nigol <thetrollfacecompany@gmail.com>
2024-05-18 01:48:33 +0300

    Update Transition.tsx

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -2,21 +2,21 @@ import { useEffect } from "preact/hooks";
 import { PropsWithChildren, useState } from "react";
 
 export default function Transition({ children }: PropsWithChildren<{}>) {
-	const [currentChild, setCurrentChild] = useState(children);
+  const [currentChild, setCurrentChild] = useState(children);
 
-	useEffect(() => {
-		if (currentChild === children) return;
+  useEffect(() => {
+    if (currentChild === children) return;
 
-		const id = setTimeout(() => {
-			setCurrentChild(children);
-		}, 200);
+    const id = setTimeout(() => {
+      setCurrentChild(children);
+    }, 200);
 
-		return () => clearTimeout(id);
-	}, [children]);
+    return () => clearTimeout(id); // Cleanup the timeout if the component unmounts or children changes
 
-	return (
-		<div className={currentChild !== children ? "fade-out" : "fade-in"}>
-			{currentChild}
-		</div>
-	);
+  }, [children, currentChild]);
+
+  return (
+    // Render the current child
+    {currentChild}
+  );
 }
