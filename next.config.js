9241d1117de754ff302f5cbac2f234d0ad28e525
raitnigol <root@NIGOL.localdomain>
2024-05-18 03:58:37 +0300

    ig

--- next.config.js
+++ next.config.js
@@ -1,23 +1,46 @@
-// @ts-check
-
 const withPreact = require("next-plugin-preact");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-	enabled: process.env.ANALYZE === "true"
+  enabled: process.env.ANALYZE === "true",
 });
 
 /** @type {import("next").NextConfig} */
 const config = {
-	reactStrictMode: true,
-	swcMinify: true,
-	i18n: { locales: ["en-US"], defaultLocale: "en-US" },
-	images: {
-		domains: ["cdn.discordapp.com", "i.scdn.co", "skillicons.dev"],
-		dangerouslyAllowSVG: true,
-		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-	},
-	experimental: {
-		esmExternals: true,
-	}
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: { locales: ["en-US"], defaultLocale: "en-US" },
+  images: {
+    domains: ["cdn.discordapp.com", "i.scdn.co", "skillicons.dev"],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  experimental: {
+    esmExternals: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+        },
+      },
+    });
+
+    config.module.rules.push({
+      test: /\.cjs$/,
+      include: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+        },
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = withBundleAnalyzer(withPreact(config));
